e0f00869a63d4e25048874da04db0ae284d0b9f2
vargata <vargatam@yahoo.com>
2023-01-24 09:01:56 +0000

    last before removing hover

--- js/titleanim.js
+++ js/titleanim.js
@@ -8,22 +8,22 @@ titles.forEach( (item) => {
 	}
 	
 	let textIndex = 0;
-	setTimeout(spinText, 10);
+	setTimeout(spinText, 0);
 	
 	function spinText(){
 		let charIndex = 0;
-		setTimeout(spinChar,10);
+		setTimeout(spinChar, 0);
 		
 		function spinChar() {
 			item.innerText = item.innerText.substring(0, textIndex) + charset.charAt(Math.random() * charset.length - 1) + item.innerText.substring(textIndex + 1, item.innerText.length);
 			if(charIndex < 5) {
 				charIndex++;
-				setTimeout(spinChar, 10);
+				setTimeout(spinChar, 30);
 			} else {
 				item.innerText = item.innerText.substring(0, textIndex) + originalText.charAt(textIndex) + item.innerText.substring(textIndex + 1, item.innerText.length);
 				if(textIndex < originalText.length - 1){
 					textIndex++;
-					setTimeout(spinText, 10);
+					setTimeout(spinText, 0);
 				}
 			}
 		}
