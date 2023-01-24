bdac204ef497e76ff59d8fea7e00b7d7c9b2abe2
vargata <vargatam@yahoo.com>
2023-01-24 14:46:39 +0000

    final push

--- js/pfclick.js
+++ js/pfclick.js
@@ -1,61 +1,68 @@
+var vert = 0;
+var horz = 0;
+var scale = 0;
+		
+function clicked(e) {
+	if(window.innerWidth < 770){
+		vert = 0;
+		horz = 0;
+		scale = 160;
+	} else if(window.innerWidth < 1260 && window.innerWidth > 770){
+		scale = 180;
+		switch(e.currentTarget.id.charAt(e.currentTarget.id.length - 1)){
+			case "1": vert = 18; horz = 12;
+				break;
+			case "2": vert = 18; horz = -12;
+				break;
+			case "3": vert = 0; horz = 12;
+				break;
+			case "4": vert = 0; horz = -12;
+				break;
+			case "5": vert = -18; horz = 12;
+				break;
+			case "6": vert = -18; horz = -12;
+				break;			
+		}
+	} else if(window.innerWidth >= 1260){
+		scale = 220;
+		switch(e.currentTarget.id.charAt(e.currentTarget.id.length - 1)){
+			case "1": vert = 7; horz = 11;
+				break;
+			case "2": vert = 7; horz = 0;
+				break;
+			case "3": vert = 7; horz = -11;
+				break;
+			case "4": vert = -7; horz = 11;
+				break;
+			case "5": vert = -7; horz = 0;
+				break;
+			case "6": vert = -7; horz = -11;
+				break;			
+		}
+	}
+	
+	if(e.currentTarget.style.zIndex == 0){
+		e.currentTarget.classList.remove("hiddenbutton");
+		e.currentTarget.style.zIndex = "4";
+		e.currentTarget.style.position = "absolute";
+		e.currentTarget.style.transform = "scale("+scale+"%) translate("+horz+"vw, "+vert+"vh)";
+		document.getElementById("hidden_overlay").style.visibility = "visible";
+		document.getElementById("content_overlay").style.visibility = "visible";
+	} else {
+		e.currentTarget.classList.add("hiddenbutton");
+		e.currentTarget.style.zIndex = "0";
+		e.currentTarget.style.position = "static";
+		e.currentTarget.style.transform = "scale(100%) translate(0vw, 0vh)";
+		document.getElementById("hidden_overlay").style.visibility = "hidden";
+		document.getElementById("content_overlay").style.visibility = "hidden";
+	}
+}		
+
 function initWindow(){
 	for( i = 1; i <= 6; i++){
-		let top = 0;
-		let left = 0;
-		let scale = 160;
-		console.log(window);
-		
-		if(window.innerWidth < 1260 && window.innerWidth > 770){
-			scale = 180;
-			switch(i){
-				case 1: top = 18; left = 12;
-					break;
-				case 2: top = 18; left = -12;
-					break;
-				case 3: top = 0; left = 12;
-					break;
-				case 4: top = 0; left = -12;
-					break;
-				case 5: top = -18; left = 12;
-					break;
-				case 6: top = -18; left = -12;
-					break;			
-			}
-		} else if(window.innerWidth >= 1260){
-			scale = 220;
-			switch(i){
-				case 1: top = 7; left = 11;
-					break;
-				case 2: top = 7; left = 0;
-					break;
-				case 3: top = 7; left = -11;
-					break;
-				case 4: top = -7; left = 11;
-					break;
-				case 5: top = -7; left = 0;
-					break;
-				case 6: top = -7; left = -11;
-					break;			
-			}
-		}
 		document.getElementById("content" + i).classList.add("hiddenbutton");
-		
-		document.getElementById("content" + i).addEventListener("click", (e) => {
-			if(e.currentTarget.style.zIndex == 0){
-				e.currentTarget.classList.remove("hiddenbutton");
-				e.currentTarget.style.zIndex = "1";
-				e.currentTarget.style.position = "absolute";
-				e.currentTarget.style.transform = "scale("+scale+"%) translate("+left+"vw, "+top+"vh)";
-			} else {
-				e.currentTarget.classList.add("hiddenbutton");
-				e.currentTarget.style.zIndex = "0";
-				e.currentTarget.style.position = "absolute";
-				e.currentTarget.style.transform = "scale(100%) translate(0vw, 0vh)";
-			}
-		});
+		document.getElementById("content" + i).addEventListener("click", clicked);
 	}
 }
-
-window.addEventListener("resize", initWindow);
 
 initWindow();
